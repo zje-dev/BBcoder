5353427928cb452ad36d2733c523230d8b2c96a1
= <juancaxy13@gmail.com>
2019-08-18 19:27:42 -0500

    alpha 1.1

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
-var element = null;
-var seleccet = false;
-function config () {
-
+window.onload = function () {
+	function EA () {
+		document.getElementById("0").style = "display: none";
+		document.getElementById("1").style = "display: none";
+		document.getElementById("2").style = "display: none";
+		document.getElementById(document.getElementById("EV").options[document.getElementById("EV").selectedIndex].value).style = "display: block"; 
+	}
+	document.getElementById("EV").onchange = EA;
 }
-document.addEventListener('DOMContentLoaded',config);
