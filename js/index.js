bf42bd602cce1244970c5eaf2f5496095e0208eb
= <juancaxy13@gmail.com>
2019-08-31 20:44:04 -0500

    alpha 1.2

--- js/index.js
+++ js/index.js
@@ -5,5 +5,26 @@ window.onload = function () {
 		document.getElementById("2").style = "display: none";
 		document.getElementById(document.getElementById("EV").options[document.getElementById("EV").selectedIndex].value).style = "display: block"; 
 	}
+	EA();
 	document.getElementById("EV").onchange = EA;
+	function EA2 () {
+		document.getElementById("C").style = "display: none";
+		document.getElementById("T").style = "display: none";
+		document.getElementById("S").style = "display: none";
+		document.getElementById(document.getElementById("ET").options[document.getElementById("ET").selectedIndex].value).style = "display: block"; 
+		var ActiveElement = document.getElementById("ET").options[document.getElementById("ET").selectedIndex].value;
+		console.log(ActiveElement);
+		function EditColor () {
+			let val = "[color=#"+document.getElementById("IC").value+"]"+document.getElementById("ETT").value+"[/color]";
+			document.getElementById("in_the_end").value = document.getElementById("in_the_end").value + val;
+			console.log(val);
+		}
+		switch (ActiveElement) {
+			case "C":
+				document.getElementById("ETB").onclick = EditColor;
+				break;
+		}
+	}
+	EA2();
+	document.getElementById("ET").onchange = EA2;
 }
